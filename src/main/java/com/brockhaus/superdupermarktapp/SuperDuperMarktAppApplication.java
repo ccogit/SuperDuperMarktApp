4dbcee3600271e491d88a919318f2b9e6fda6d9e
chris <christopher.ridder@gmail.com>
2023-12-04 19:09:52 +0100

    Initial commit of project.

---
+++ src/main/java/com/brockhaus/superdupermarktapp/SuperDuperMarktAppApplication.java
@@ -0,0 +1,13 @@
+package com.brockhaus.superdupermarktapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperDuperMarktAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperDuperMarktAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brockhaus/superdupermarktapp/SuperDuperMarktAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brockhaus.superdupermarktapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperDuperMarktAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
