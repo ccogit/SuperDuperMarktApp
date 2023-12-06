79ad3d65b600000e7959b954c1666e82a0a56cc7
chris <christopher.ridder@gmail.com>
2023-12-06 09:59:29 +0100

    EurekaServer added.

--- EurekaServer/src/test/java/com/brockhaus/eurekaserver/EurekaServerApplicationTests.java
+++ EurekaServer/src/test/java/com/brockhaus/eurekaserver/EurekaServerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.brockhaus.superdupermarktapp;
+package com.brockhaus.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SuperDuperMarktAppApplicationTests {
+class EurekaServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
